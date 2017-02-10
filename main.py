765ed4cad72fdc6b916d8d5023bd0a115405ffc5
mjbright <github.com@mjbright.net>
2017-02-10 14:32:26 +0100

    Testing if I can embed a remark.js presentation

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
         prefix = "/python/lib/python3.5/site-packages/"
         if self.path == '/':
             self.path = prefix + 'index.html'
+        elif self.path == '/SNOWCAMP':
+            self.path = prefix + '2017-Feb-10_Snowcamp.io_Unikernels/2017-Feb-10_Snowcamp.io_Unikernels.md.html'
         else:
             self.path = prefix + self.path
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
